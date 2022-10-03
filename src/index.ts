d2f2a5be4da81bc3e905f83cb60b914ff88627de
Vahid V <vhid.vz@gmail.com>
2022-10-03 13:59:39 +0330

    feat: add case conventions method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
-/**
- * This comment _supports_ [Markdown](https://marked.js.org/)
- */
-export function HelloWorld(): string {
-  return 'Hello World...!';
+export function toNoCase(str: string): string {
+  return str
+    .replace(/[A-Z][a-z]+/g, (m) => ' ' + m)
+    .replace(/[A-Z][A-Z]+/g, (m) => ' ' + m)
+    .replace(/[\s-_]+/g, ' ')
+    .toLowerCase()
+    .trim();
+}
+
+export function toSnakeCase(str: string): string {
+  return toNoCase(str).replace(/\s/g, '_');
+}
+
+export function toCamelCase(str: string): string {
+  return toNoCase(str)
+    .replace(/[^\w][a-z]/g, (m) => m.toUpperCase())
+    .replace(/\s/g, '');
+}
+
+export function toPascalCase(str: string): string {
+  return toNoCase(' ' + str)
+    .replace(/[^\w][a-z]/g, (m) => m.toUpperCase())
+    .replace(/\s/g, '');
+}
+
+export function toMacroCase(str: string): string {
+  return toNoCase(str)
+    .replace(/[a-z]+/g, (m) => m.toUpperCase())
+    .replace(/\s/g, '_');
+}
+
+export function toKebabCase(str: string): string {
+  return toNoCase(str).replace(/\s/g, '-');
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,25 @@
-import { HelloWorld } from '../src';
+import { toMacroCase } from '../src';
+
+const strings = [
+  'RegExr',
+  'PCRE',
+  'JavaScript',
+  'JSProgrammingLanguage',
+  'OTP',
+  'camelCase',
+  '_id',
+  'ID',
+  'iD',
+  'id',
+  'Id',
+  '0123',
+  '_-$#@',
+];
 
 describe('test hello function', () => {
   it("should return 'Hello World...!'", () => {
-    expect(HelloWorld()).toBe('Hello World...!');
+    for (const str of strings) {
+      console.log(toMacroCase(str));
+    }
   });
 });
